72e8b442dbed02483bd6492f655affdb84530f1e
MILKAA <kameel.2011@icloud.com>
2026-04-21 16:21:07 +0200

    Add files and first codes with Tkinter

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Panda Engine")
+root.geometry("1080x720")
+root.minsize(480, 360)
+# root.iconbitmap("assets/img/panda.ico") 
+root.config(background='#1e1e1e')
+label_title = Label(root, text="Panda Engine Wallpaper", font=("Times New Roman", 40))
+label_title.pack()
+
+
+
+
+
+
+root.mainloop()
